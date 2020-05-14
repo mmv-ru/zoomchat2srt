5acebf9a99af4ff509975fb9f24c0a2dce9aecc4
MMV <mmv.rus@gmail.com>
2020-05-14 04:08:13 +0300

    First try

---
+++ zoomchat2srt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+text_file = open("/mnt/media3/20200513 Защита прав по лекарственному обеспечению/GMT20200513-140640_----------.txt", "r")
+lines = text_file.readlines()
+print lines
+foreach line in lines:
+  pass
+text_file.close()
